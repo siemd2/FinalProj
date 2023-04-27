029c8202cd78a5169ce9ddb859bcc0b395a72241
Tommy-La <tien.la@edu.sait.ca>
2023-04-27 15:24:14 -0600

    Added SQL scripts

---
+++ FinalProj/Resources/SQLScript/SQLScriptForDB.sql
@@ -0,0 +1,48 @@
+INSERT INTO FP_staff VALUES(51348, 'Tech4', null, null);
+INSERT INTO FP_STAFF VALUES(53546, 'Tech2', null, null);
+INSERT INTO FP_STAFF VALUES(54632, 'Tech1', null, null);
+INSERT INTO FP_STAFF VALUES(55314, 'Manager', null, null);
+INSERT INTO FP_STAFF VALUES(59874, 'Front Desk', null, null);
+
+
+INSERT INTO FP_SERVICE VALUES(14567, 'Oil Change', 35.00, 45, 'Replacement of engine oil and oil filter.');
+
+INSERT INTO FP_SERVICE VALUES(14568, 'Brake Service', 180.00, 120, 'Replacement of brake pads and brake fluid.');
+
+INSERT INTO FP_SERVICE VALUES(14569, 'Tire Rotation', 40.00, 30, 'Rotating the tires to promote even wear and extend tire life.');
+
+INSERT INTO FP_SERVICE VALUES(14570, 'Transmission Service', 250.00, 120, 'Changing the transmission fluid and filter.');
+
+INSERT INTO FP_SERVICE VALUES(14571, 'Battery Replacement', 100.00, 30, 'Replacement of the vehicles battery.');
+
+INSERT INTO FP_SERVICE VALUES(14572, 'Air Conditioning Service', 150.00, 90, 'Checking and refilling the AC refrigerant and replacing the AC compressor if necessary.');
+
+INSERT INTO FP_SERVICE VALUES(14573, 'Cooling System Service', 80.00, 60, 'Flushing the cooling system and replacing the coolant.');
+
+
+INSERT INTO FP_Customer VALUES (40001, 'John Doe', 123045679, 'johndoe@gmail.com', '123 Main St');
+
+INSERT INTO FP_Customer VALUES(40002, 'Jane Smith', 987605431, 'janesmith@hotmail.com', '456 Oak Ave');
+
+INSERT INTO FP_Customer VALUES(40003, 'Bob Johnson', 555123467, 'bob.johnson@yahoo.com', '789 Elm St');
+
+INSERT INTO FP_Customer VALUES(40004, 'Mary Williams', 555986543, 'marywilliams@gmail.com', '234 Pine Rd');
+
+INSERT INTO FP_Customer VALUES(40005, 'Tom Davis', 999888777, 'tomdavis@hotmail.com', '567 Maple Ave');
+
+
+
+INSERT INTO FP_WorkTicket VALUES (01234, null, 40001, 14573, '12/12/22');
+
+INSERT INTO FP_VehiclePart VALUES (03214, 'Battery', 0, null, '11-30-23', 'tysa3122012');
+
+INSERT INTO FP_VehiclePart VALUES (03315, 'Alternator', 0, null, '11-20-23', 'tysa3122012');
+
+INSERT INTO FP_VehiclePart VALUES (03226, 'Fuel Pump', 0, null, '03-12-23', 'tysa3122012');
+
+
+INSERT INTO FP_MiscItem Values (54624, 'Lubricants', 20, 'New');
+
+INSERT INTO FP_MiscItem Values (54824, 'Oil', 29, 'New');
+
+INSERT INTO FP_MiscItem Values (54664, 'Hoists and lifts', 3, 'Need Repair');
